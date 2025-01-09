f0eaad50b965056791527346f34da1cda1a8bb27
chevylevel <chevylevel@gmail.com>
2025-01-09 19:28:16 +0800

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   resolve: {
@@ -61,7 +61,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: './dist',
+    static: './build',
     open: true,
     hot: true,
   },
