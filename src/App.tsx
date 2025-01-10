044c737e7a75c39f648909da335a73476706b3b2
chevylevel <chevylevel@gmail.com>
2025-01-10 14:42:37 +0800

    fix unwanted mobile scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
@@ -6,8 +6,23 @@ gsap.registerPlugin(useGSAP);
 
 import PageContainer from './components/PageContainer/PageContainer';
 
-const App: FC = () => (
-    <PageContainer />
-);
+const App: FC = () => {
+    const updateViewportHeight = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    useLayoutEffect(() => {
+        updateViewportHeight();
+
+        window.addEventListener('resize', updateViewportHeight);
+
+        return () => {
+            window.removeEventListener('resize', updateViewportHeight);
+        };
+    }, []);
+
+    return <PageContainer />
+}
 
 export default App;
--- src/components/PageLayout/styled/mobile/StyledPageLayoutMobile.ts
+++ src/components/PageLayout/styled/mobile/StyledPageLayoutMobile.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 const pageLayoutMobileStyles = () => css`
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
     padding: 0 20px;
 
     &::before {
